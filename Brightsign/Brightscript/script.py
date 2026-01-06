dbe7311526a8546319266b00f4c763c661110b84
A <86937359+azuell@users.noreply.github.com>
2026-01-06 16:57:09 +1100

    Add events and actions. All work except volume

--- Brightsign/Brightscript/script.py
+++ Brightsign/Brightscript/script.py
@@ -1,7 +1,9 @@
 '''
 **Brightsign** for use with Nodel Brightsign Plugin
 
-`rev 4` rewite of original script by Troy Takac
+`rev 4` (rewrite of original script by Troy Takac)
+
+azuell 06/01/26
 
 Includes:
 
@@ -9,7 +11,7 @@ Includes:
 
 **REVISION HISTORY**
 
-* 
+* rev 4: complete rewrite. remove need for socket 
 
 '''
 
@@ -27,17 +29,20 @@ local_event_Model = LocalEvent({'group': 'Information', 'order': next_seq(), 'sc
 local_event_Serial = LocalEvent({'group': 'Information', 'order': next_seq(), 'schema': {'type': 'string'}})
 local_event_VideoMode = LocalEvent({'group': 'Information', 'order': next_seq(), 'schema': {'type': 'string'}})
 
-local_event_Power = LocalEvent({'group': 'Power', 'order': next_seq(), 'schema': {'type': 'string', 'enum': ['On', 'Off']}, 'desc': 'Power State'})
+local_event_Power = LocalEvent({'group': 'Power', 'order': next_seq(), 'schema': {'type': 'string', 'enum': ['On', 'Partially On', 'Off', 'Partially Off']}, 'desc': 'Power State'})
+local_event_RawPower = LocalEvent({'group': 'Power', 'order': next_seq(), 'schema': {'type': 'string', 'enum': ['On', 'Off']}, 'desc': 'Raw Power State'})
 local_event_DesiredPower = LocalEvent({'group': 'Power', 'order': next_seq(), 'schema': {'type': 'string', 'enum': ['On', 'Off']}, 'desc': 'Desired Power State'})
 
-local_event_Volume = LocalEvent({'group': 'Volume', 'order': next_seq(), 'schema': {'type': 'string'}})
-local_event_Mute = LocalEvent({'group': 'Volume', 'order': next_seq(),'schema': {'type': 'string', 'enum': ['On', 'Off']}, 'desc': 'Mute State'})
+local_event_Mute = LocalEvent({'group': 'Volume', 'order': next_seq(),'schema': {'type': 'string', 'enum': ['On', 'Partially On', 'Off', 'Partially Off']}, 'desc': 'Mute State'})
+local_event_RawMute = LocalEvent({'group': 'Volume', 'order': next_seq(),'schema': {'type': 'string', 'enum': ['On', 'Off']}, 'desc': 'Raw Mute State'})
 local_event_DesiredMute = LocalEvent({'group': 'Volume', 'order': next_seq(),'schema': {'type': 'string', 'enum': ['On', 'Off']}, 'desc': 'Desired Mute State'})  
+local_event_Volume = LocalEvent({'group': 'Volume', 'order': next_seq(), 'schema': {'type': 'integer'}})
+local_event_RawVolume = LocalEvent({'group': 'Volume', 'order': next_seq(), 'schema': {'type': 'integer'}})
+local_event_DesiredVolume = LocalEvent({'group': 'Volume', 'order': next_seq(), 'schema': {'type': 'integer'}})
 
 local_event_Playback = LocalEvent({'group': 'Playback', 'order': next_seq(),'schema': {'type': 'string'}, 'desc': 'Playback State'})
+local_event_RawPlayback = LocalEvent({'group': 'Playback', 'order': next_seq(),'schema': {'type': 'string'}, 'desc': 'Raw Playback State'})
 local_event_DesiredPlayback = LocalEvent({'group': 'Playback', 'order': next_seq(),'schema': {'type': 'string'}, 'desc': 'Desired Playback State'})
-
-import sys
 
 def main():
   if param_disabled:
@@ -45,8 +50,6 @@ def main():
     
   if is_blank((param_playerConfig or {}).get('ipAddress')):
     return console.warn('IP address not specified!')
-  
-  
     
     # console.info('Polling will start in %ss' % _timer_sync.getDelay())
     # _timer_sync.start()
@@ -73,6 +76,104 @@ def main():
 
 #   console.log("Brightsign script started.")
 
+@local_action({'group': 'Power', 'order': next_seq(), 'schema': {'type': 'string', 'enum': ['On', 'Off']}})           
+def Power(arg):
+  if arg == 'On':
+    local_event_DesiredPower.emit('On')
+    callURL('playback?sleep=false')
+  elif arg == 'Off':
+    local_event_DesiredPower.emit('Off')
+    callURL('playback?sleep=true')
+
+@local_action({'group': 'Power', 'title': 'On', 'order': next_seq()})  
+def PowerOn(arg = None):
+  Power.call('On')
+
+@local_action({'group': 'Power', 'title': 'Off', 'order': next_seq()})  
+def PowerOff(arg = None):
+  Power.call('Off')
+
+@local_action({'group': 'Power', 'title': 'Reboot', 'order': next_seq()})  
+def Reboot():
+  callURL('reboot?reboot=true')
+
+@local_action({'group': 'Volume', 'title': 'Mute', 'order': next_seq(), 'schema': {'type': 'string', 'enum': ['On', 'Off']}})  
+def Mute(arg):
+  if arg == 'On':
+    local_event_DesiredMute.emit('On')
+    callURL('mute?mute')
+
+  elif arg == 'Off':
+    local_event_DesiredMute.emit('Off')
+    callURL('mute?unmute')
+
+@local_action({'group': 'Volume', 'title': 'Mute On', 'order': next_seq()})
+def MuteOn():
+  Mute.call('On')
+  
+@local_action({'group': 'Volume', 'title': 'Mute Off', 'order': next_seq()})
+def MuteOff():
+  Mute.call('Off') 
+
+@local_action({'group': 'Volume', 'title': 'Volume', 'order': next_seq(), 'schema': { 'type': 'integer', 'hint': '(0 - 100%)' }})
+def Volume(arg):
+    if arg == None or arg < 0 or arg > 100:
+      console.warn('Volume: no arg or outside 0 - 100')
+      return
+    local_event_DesiredVolume.emit(arg)
+    callURL("volume?%s" % str(arg))
+
+@local_action({'group': 'Playback', 'title': 'Playback', 'order': next_seq(), 'schema': {'type': 'string', 'enum': ['On', 'Off']}})  
+def Playback(arg):
+  if arg == 'On':
+    local_event_DesiredPlayback.emit('On')
+    callURL('playback?playback=play')
+
+  elif arg == 'Off':
+    local_event_DesiredPlayback.emit('Off')
+    callURL('playback?playback=pause')
+
+@local_action({'group': 'Playback', 'title': 'Play', 'order': next_seq()})  
+def PlaybackOn():
+  Playback.call('On')
+
+@local_action({'group': 'Playback', 'title': 'Pause', 'order': next_seq()})  
+def PlaybackOff():
+  Playback.call('Off')
+
+
+# When desired or raw statuses change, update the main status accordingly (using "Partially" if theres a mismatch)
+@after_main
+def combineFeedback():
+    def statusComparison(desiredStatus, rawStatus):
+        if desiredStatus == None:           return rawStatus
+        elif desiredStatus == rawStatus:    return rawStatus
+        else:                               return 'Partially %s' % desiredStatus
+
+    def powerHandler(arg):
+      power = statusComparison(local_event_DesiredPower.getArg(), local_event_RawPower.getArg())
+      local_event_Power.emit(power)
+    local_event_DesiredPower.addEmitHandler(powerHandler)
+    local_event_RawPower.addEmitHandler(powerHandler)
+
+    def muteHandler(arg):
+      mute = statusComparison(local_event_DesiredMute.getArg(), local_event_RawMute.getArg())
+      local_event_Mute.emit(mute)
+    local_event_DesiredMute.addEmitHandler(muteHandler)
+    local_event_RawMute.addEmitHandler(muteHandler)
+
+    def playbackHandler(arg):
+      playback = statusComparison(local_event_DesiredPlayback.getArg(), local_event_RawPlayback.getArg())
+      local_event_Mute.emit(playback)
+    local_event_DesiredPlayback.addEmitHandler(playbackHandler)
+    local_event_RawPlayback.addEmitHandler(playbackHandler)
+
+    def volumeHandler(arg):
+      volume = statusComparison(local_event_DesiredVolume.getArg(), local_event_RawVolume.getArg())
+      local_event_Volume.emit(volume)
+    local_event_DesiredVolume.addEmitHandler(volumeHandler)
+    local_event_RawVolume.addEmitHandler(volumeHandler)
+
 # udp = UDP()
 
 # def SendUDP(message):
@@ -92,7 +193,7 @@ def main():
 
 _busy = False
 
-def call(command, forceLog=False, method=None, query=None, contentType=None, post=None, fullResponse=False):
+def callURL(command, forceLog=False, method=None, query=None, contentType=None, post=None):
     # Avoid simultaneous calls by tracking one at a time
     global _busy
     if _busy:
@@ -109,47 +210,61 @@ def call(command, forceLog=False, method=None, query=None, contentType=None, pos
         try:
             timestamp = system_clock()
             # get_url(url, method=None, query=None, username=None, password=None, headers=None, contentType=None, post=None, connectTimeout=10, readTimeout=15, fullResponse=False)
-            resp = get_url(url, method=method, query=query, contentType=None, post=post, connectTimeout=5, readTimeout=5, fullResponse=fullResponse)
-        except:
-            e = sys.exc_info()[1]   # Tuple order is excType, value, trace
+            resp = get_url(url, method=method, query=query, contentType=None, post=post, connectTimeout=5, readTimeout=5, fullResponse=True)
+
+            if resp.statusCode != 200:
+              raise Exception(str(resp.statusCode) + " Error: " + str(resp.reasonPhrase))
+
+        except Exception, e:
+            console.warn(resp.statusCode)
+            #e = sys.exc_info()[1]   # Tuple order is excType, value, trace
             msg = 'get_url: failed (took %0.1f) with "%s"' % ((system_clock()-timestamp)/1000.0, e)
 
             if forceLog: console.warn(msg)
-            else:       warn(1, msg)
+            else:        warn(1, msg)
 
             return False
 
-        log(1, 'resp: %s' % resp)
+        log(1, 'resp: %s' % resp.content)
 
         global _lastReceive
         _lastReceive = system_clock()
 
-        return resp
+        return resp.content
     
     finally:
         _busy = False
 
-@local_action({'title': 'test full resp'})
-def test():
-  resp = call('status', forceLog=True, method='GET', contentType='application/json', fullResponse=True)
-  console.log(resp)
-
 @local_action({'title': 'Get Info', 'order': next_seq()})
 def status():
-  resp = call('status', forceLog=True, method='GET', contentType='application/json')
-  console.log(resp)
-  status_decode = json_decode(resp)
-
-  lookup_local_event('Model').emit(status_decode['model'])
+  resp = callURL('status', forceLog=True, method='GET', contentType='application/json')
   
-  lookup_local_event('Serial').emit(status_decode['serialNumber'])
-  lookup_local_event('VideoMode').emit(status_decode['videomode'])
+  if resp:  status = json_decode(resp) 
+  else:     return
 
-  lookup_local_event('Volume').emit(status_decode['volume'])
-  #muted: false
+  local_event_Model.emit(status['model'])
+  local_event_Serial.emit(status['serialNumber'])
+  local_event_VideoMode.emit(status['videomode'])
 
-  #playing: true
-  #sleep: false
+  local_event_RawVolume.emit(int(status['volume']))
+
+  if status['sleep'] == 'true':
+    local_event_RawPower.emit('Off')
+  if status['sleep'] == 'false':
+    local_event_RawPower.emit('On')
+
+  if status['mute'] == 'true':
+    local_event_RawMute.emit('On')
+  if status['mute'] == 'false':
+    local_event_RawMute.emit('Off')
+
+  if status['playing'] == 'true':
+    local_event_RawPlayback.emit('On')
+  if status['playing'] == 'false':
+    local_event_RawPlayback.emit('Off')
+
+  
+
 
 
 ### Status and Error Reporting
