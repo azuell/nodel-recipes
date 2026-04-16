8b0b5f649c027fef52992260ad1bd6b371087f28
A <86937359+azuell@users.noreply.github.com>
2026-04-16 14:25:37 +1000

    Create script.py

---
+++ Show Control/script.py
@@ -0,0 +1,37 @@
+'''
+**Show Control** 
+
+Use case: condense multiple OSC Agent node actions into one action with arguments and state, to be used on a Frontend node with < select > object
+
+`REV 1 2026.04.16`
+
+**REVISION HISTORY**
+
+* rev. 1: created
+'''
+
+param_states = Parameter({'title': 'Show States', 'order': next_seq(), 'schema': {'type': 'array', 'items': {
+  'type': 'object', 'title': 'State', 'properties': {
+    'state': {'type': 'string', 'hint': 'Standard'}}}}})
+
+param_node = Parameter({'title': 'Suggested Node', 'schema': {'type': 'string', 'hint': 'OSC Client'}})
+
+def main():
+  console.info("Recipe has started!")
+  
+  if not is_empty(param_states):
+    initRemoteActions()
+    initLocalAction()
+  
+def initLocalAction():
+  
+  def handler(arg):
+    lookup_remote_action(arg).call()
+    lookup_local_event('Show').emit(arg)
+
+  create_local_action('Show', handler, {'title': 'Show', 'order': next_seq(), 'schema': {'type': 'string', 'enum': [show.get('state') for show in param_states]}})
+  create_local_event('Show', {'title': 'Show', 'order': next_seq(), 'schema': {'type': 'string', 'enum': [show.get('state') for show in param_states]}})
+
+def initRemoteActions():
+  for show in param_states:
+    create_remote_action(show.get('state'), suggestedNode=param_node, suggestedAction=show.get('state'))
